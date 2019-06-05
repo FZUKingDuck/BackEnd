943be25ac10dde414290381199ecfd765af327e6
叫猪学猪叫 <974493041@qq.com>
2019-06-05 17:39:25 +0800

    获取帖子的列表分页查询方法

--- Forum/src/main/java/com/forum/demo/Controller/PostsController.java
+++ Forum/src/main/java/com/forum/demo/Controller/PostsController.java
@@ -2,18 +2,25 @@ package com.forum.demo.Controller;
 
 
 import com.forum.demo.DAO.CustomDao;
+import com.forum.demo.DAO.PostsDao;
+import com.forum.demo.DAO.TaskDao;
 import com.forum.demo.Entity.CustomEntity;
+import com.forum.demo.Entity.PostsEntity;
 import com.forum.demo.ResponseResult.Result;
 import com.forum.demo.UtilTool.DateUtil;
 import com.forum.demo.UtilTool.RedisOperator;
 import com.forum.demo.UtilTool.StringUtils;
+import org.hibernate.validator.constraints.EAN;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.websocket.server.PathParam;
+import java.util.List;
 
 @RestController
 @RequestMapping(value =  "/posts")
@@ -25,8 +32,52 @@ public class PostsController {
     @Autowired
     RedisOperator redisOperator;
 
+    @Autowired
+    PostsDao postsDao;
+
+    @Autowired
+    TaskDao taskDao;
 
 
+    //获取帖子的列表
+    //type：帖子的类型    pageNumber：页码
+    //返回指定类型的帖子的数组
+    @GetMapping(value = "getPostsList")
+    public Result getPostsList(@PathParam("type")String type,@PathParam("pageNumber")String pageNumber){
+        Result result = new Result();
+
+        if(!StringUtils.checkKey(type)||!StringUtils.checkKey(pageNumber)){
+            result.setNullFalse();
+            return result;
+        }
+
+        try{
+            int number = Integer.valueOf(pageNumber);
+            if(number<1){
+                result.setFalse(201,"页码错误");
+                return result;
+            }
+
+            //创建根据发帖时间排序的分页查询规则
+            PageRequest page = PageRequest.of(number,10, Sort.Direction.DESC,"creattime");
+            //获取数据，返回
+            List<PostsEntity> list = postsDao.findAllByTypeIn(type,page);
+            result.setOK("ok",list);
+            return result;
+
+        }
+        catch (Exception e){
+            e.printStackTrace();
+            result.setSysFalse();
+            return result;
+        }
+
+    }
+
+    //获取所有帖子的数据
+    //不需要指定帖子的类型
+    //pageNumber：页码
+   // @GetMapping(value = "getPostsListAll")
 
 
 }
--- Forum/src/main/java/com/forum/demo/DAO/PostsDao.java
+++ Forum/src/main/java/com/forum/demo/DAO/PostsDao.java
@@ -2,7 +2,14 @@ package com.forum.demo.DAO;
 
 import com.forum.demo.Entity.ClassInfoEntity;
 import com.forum.demo.Entity.PostsEntity;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface PostsDao extends JpaRepository<PostsEntity,String> {
+    public List<PostsEntity> findAllByTypeIn(String type, Pageable page);
+
 }
