7c60647bbb64a8c1ebf4c11af190cb473c2ff7bb
叫猪学猪叫 <974493041@qq.com>
2019-06-05 19:42:29 +0800

    截图工具

--- Forum/src/main/java/com/forum/demo/Controller/PostsController.java
+++ Forum/src/main/java/com/forum/demo/Controller/PostsController.java
@@ -10,17 +10,20 @@ import com.forum.demo.ResponseResult.Result;
 import com.forum.demo.UtilTool.DateUtil;
 import com.forum.demo.UtilTool.RedisOperator;
 import com.forum.demo.UtilTool.StringUtils;
+import javafx.geometry.Pos;
 import org.hibernate.validator.constraints.EAN;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
+import org.springframework.util.unit.DataUnit;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.websocket.server.PathParam;
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
@@ -44,7 +47,7 @@ public class PostsController {
     //获取帖子的列表
     //type：帖子的类型    pageNumber：页码
     //返回指定类型的帖子的数组
-    @GetMapping(value = "getPostsList")
+    @GetMapping(value = "/getPostsList")
     public Result getPostsList(@PathParam("type")String type,@PathParam("pageNumber")String pageNumber){
         Result result = new Result();
 
@@ -79,7 +82,7 @@ public class PostsController {
     //获取所有帖子的数据
     //不需要指定帖子的类型
     //pageNumber：页码
-    @GetMapping(value = "getPostsListAll")
+    @GetMapping(value = "/getPostsListAll")
     public Result getPostsListAll(@PathParam("pageNumber")String pageNumber){
         Result result  = new Result();
 
@@ -117,8 +120,33 @@ public class PostsController {
     }
 
     //获取指定帖子的信息
-    @GetMapping(value = "getPostsInfo")
-    public Result getPosts()
+    @GetMapping(value = "/getPostsInfo")
+    public Result getPostsInfo(@PathParam("id")String id) {
+        Result result = new Result();
+
+        if(StringUtils.checkKey(id)){
+            result.setNullFalse();
+            return result;
+        }
+
+
+        Optional<PostsEntity> res = postsDao.findById(id);
+        if(res==null||!res.isPresent()){
+            result.setFalse(201,"无此帖子");
+            return result;
+
+        }
+        PostsEntity postsEntity = res.get();
+
+        result.setOK("获取成功",postsEntity);
+        return result;
+
+    }
+
+    //发帖
+    @PostMapping(value = "/newPosts")
+    public Result newPosts(@PathParam(""))
+
 
 
 }
